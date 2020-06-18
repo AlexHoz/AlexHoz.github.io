5a8cbe4381c18fb619e2448139ca6aa456b8b841
AlexHoz <alexhozov00@gmail.com>
2020-06-19 00:07:20 +0500

    Дополнил скрипт, изменил json

--- Site/script.js
+++ Site/script.js
@@ -7,26 +7,45 @@ request.send();
 
 request.onload = function() {
   var product = request.response;
-  loadProd(product, 'task_1');
+  loadProd(product, 'task_1_name','task_1');
 }
 
 
-function loadProd(jsonObj, name) {
-  var product = jsonObj[name];
-      
+function loadProd(jsonObj, name, nameCategory) {
+  var product = jsonObj[nameCategory];
+  let htmlStr='';
+  htmlStr += '<p class="nameCategory">' + jsonObj[name] +'</p>';
+  htmlStr += '<div class="Category">';
   for (var i = 0; i < product.length; i++) {
-    let htmlStr='';
+    
     htmlStr += '<div class="block">';
     htmlStr += '<div class="desP">';
     htmlStr += '<img id="desPImg" src='+product[i].img+'></img>';
     htmlStr += '<p>'+product[i].name+'</p>';
-    htmlStr += '</div><div class="buttonP">';
+    htmlStr += '</div>';
+    htmlStr += '<div class="buttonP">';
     htmlStr += '<button class="" type="button">';
     htmlStr += '<a href="">Получить</a>';
     htmlStr += '</button>';
     htmlStr += '</div> </div>';
-    Main.innerHTML+= htmlStr;
+    
   }
+  htmlStr += '</div>'
+  Main.innerHTML+= htmlStr;
 }
 
 //request.onload();
+/*
+        
+            
+            <div class="block">
+                <div class="desP">
+                    <img id="desPImg" src="img/ProjectPreview/task1(0).png" alt="task1"></img>
+                    <p>Решалка кв. уравнений (для консоли)</p>
+                </div>
+                <div class="buttonP">
+                    <button class="" type="button"><a href="">Получить</a></button>
+                </div>
+            </div>
+        </div>
+*/
